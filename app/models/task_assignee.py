c61b7a6c8e633e72568cbd88089b19629cc0b12a
user01837 <aead01837@gmail.com>
2026-07-29 18:20:24 +0900

    관리자 홈화면

--- app/models/task_assignee.py
+++ app/models/task_assignee.py
@@ -5,6 +5,6 @@ class TaskAssignee(Base):
     __tablename__ = "TASK_ASSIGNEE"
 
     task_assignee_id = Column(Integer, primary_key=True, autoincrement=True)
-    task_id = Column(Integer, ForeignKey("TASK.task_id"), nullable=False)   # 👈 "task" -> 대문자 "TASK"로 수정
-    user_id = Column(String(50), ForeignKey("USER.user_id"), nullable=False) # 👈 "user" -> 대문자 "USER"로 수정
+    task_id = Column(Integer, ForeignKey("TASK.task_id"), nullable=False)   
+    user_id = Column(String(50), ForeignKey("USER.user_id"), nullable=False) 
     assigned_at = Column(DateTime, nullable=False, server_default=func.now())
